5ed0fcc0c42b983fc998979ef9f96b18f13dcf35
Smulligan85 <sean.mulligan85@gmail.com>
2016-03-19 11:45:14 -0700

    Configure application

---
+++ spec/application_controller_spec.rb
@@ -0,0 +1,11 @@
+require_relative "spec_helper"
+
+describe ApplicationController do
+  describe "Homepage" do 
+    it 'loads the homepage' do 
+      get '/'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome")
+    end
+  end
+end
