1e1be4336cd0c02bce608791836130a45cfe97fc
Smulligan85 <sean.mulligan85@gmail.com>
2016-03-25 07:46:12 -0700

    Add validations to models

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,4 @@
 class Part < ActiveRecord::Base
+  validates_presence_of :name, :price
   belongs_to :project
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < AvtiveRecord::Base
+  validates_presence_of :title, :budget
   belongs_to :user
   has_many :parts
 end
