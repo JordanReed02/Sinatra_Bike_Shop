d9f888711b811e1278ad4101ffc70f3d3f2fceb9
Smulligan85 <sean.mulligan85@gmail.com>
2016-03-25 21:40:38 -0700

    Add signin signup logic

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,78 @@
 class ProjectsController < ApplicationController
+  get '/projects' do
+    if logged_in?
+      @projects = Project.all
+      erb :'projects/index'
+    else
+      erb :'users/login', locals: {message: "You don't have access, please login"} 
+    end
+  end
+
+  get '/projects/new' do
+    if logged_in?
+      erb :'projects/new'
+    else
+      erb :login, locals: {message: "You don't have access, please login"}
+    end
+  end
+
+  post '/projects' do
+    if params.values.any? {|value| value == ""}
+      erb :'projects/new', locals: {message: "Your missing information!"}
+    else
+      user = User.find(session[:user_id])
+      @project = Project.create(title: params[:title], budget: params[:budget], user_id: user.id)
+      redirect to "/projects/#{@project.id}"
+    end
+  end
+
+  get '/projects/:id' do 
+    if logged_in?
+      @project = Project.find(params[:id])
+      erb :'projects/show'
+    else 
+      erb :login, locals: {message: "You don't have access, please login"}
+    end
+  end
+
+  get '/projects/:id/edit' do
+    if logged_in?
+      @project = Project.find(params[:id])
+      if @project.user_id == session[:user_id]
+       erb :'projects/edit'
+      else
+      erb :projects, locals: {message: "You don't have access to edit this project"}
+      end
+    else
+      erb :login, locals: {message: "You don't have access, please login"}
+    end
+  end
+
+  patch '/projects/:id' do 
+    if params.values.any? {|value| value == ""}
+      redirect to "/projects/#{params[:id]}/edit"
+    else
+      @project = Project.find(params[:id])
+      @project.title = params[:title]
+      @project.budget = params[:budget]
+      @project.save
+      redirect to "/projects/#{@project.id}"
+    end
+  end
+
+  delete '/projects/:id/delete' do 
+    @project = Project.find(params[:id])
+    if session[:user_id]
+      @project = Project.find(params[:id])
+      if @project.user_id == session[:user_id]
+        @project.delete
+        redirect to '/projects'
+      else
+        redirect to '/projects'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
-class Project < AvtiveRecord::Base
+class Project < ActiveRecord::Base
   validates_presence_of :title, :budget
   belongs_to :user
   has_many :parts
